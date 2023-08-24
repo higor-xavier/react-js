daa3015c2c6b1499ef48c210348f720419078d7b
higor-xavier <higorsxavier@gmail.com>
2023-08-24 15:35:29 -0300

    Automating Post

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,6 +4,42 @@ import { Sidebar } from './components/Sidebar';
 import styles from './App.module.css';
 import './global.css';
 
+//author: {avatar_url: "", name: "", role: ""}
+//publishedAt: Date
+//content: String
+
+const posts = [
+    {
+        id: 1,
+        author: {
+            avatarUrl: "https://github.com/higor-xavier.png",
+            name: "Higor Xavier",
+            role: "Full-Stack Developer"
+        },
+        content: [
+            { type: 'paragraph', content: 'Fala galeraa 👋'},
+            { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare:'},
+            { type: 'link', content: 'jane.design/doctorcare'},     
+        ],
+        publishedAt: new Date('2023-04-20 08:40:46')
+    },
+
+    {
+        id: 2,
+        author: {
+            avatarUrl: "https://github.com/higor-xavier.png",
+            name: "Higor Xavier",
+            role: "Full-Stack Developer"
+        },
+        content: [
+            { type: 'paragraph', content: 'Fala galeraa 👋'},
+            { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare:'},
+            { type: 'link', content: 'jane.design/doctorcare'},     
+        ],
+        publishedAt: new Date('2023-08-24 05:45:46')
+    }
+];
+
 export function App() {
     return (
         <div>
@@ -12,10 +48,18 @@ export function App() {
             <div className={styles.wrapper}>
                  <Sidebar />
                  <main>
-                    <Post 
-                        author='higor x' 
-                        content='Lorem ipsum dolor sit, amet consectetur'
-                    />
+                    {
+                        posts.map( post => {
+                            return (
+                                <Post 
+                                    author = { post.author }
+                                    content = { post.content }
+                                    publishedAt = { post.publishedAt }
+
+                                />
+                            )
+                        })
+                    }
                  </main>
             </div>
             
--- vite-project/src/components/Post.jsx
+++ vite-project/src/components/Post.jsx
@@ -1,32 +1,45 @@
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
-export function Post(props){
+export function Post({author, publishedAt, content}){
+    
+    const publishedDateFormated = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+        locale: ptBR,
+    });
+
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+        locale: ptBR,
+        addSuffix: true
+    });
+
     return(
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar src="https://github.com/higor-xavier.png" />
+                    <Avatar src= {author.avatarUrl}/>
                     <div className={styles.authorInfo}>
-                        <strong>{props.author}</strong>
-                        <span>Tal tal tal</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
 
                     </div>
                 </div>
 
-                <time title='20 de Abril às 04:20h' dateTime="2023-04-20 04:20:46">Publicado há 1h</time>
+                <time title={publishedDateFormated} dateTime={publishedAt.toISOString()} >
+                    { publishedDateRelativeToNow }
+                </time>
             </header>
 
             <div className={styles.content}>
-                <p>Fala galeraa 👋</p>
-                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare:</p>
-                <p>👉{' '}<a href="#">jane.design/doctorcare</a></p>
-                <p>
-                    <a href="#">#novoprojeto</a>{' '}
-                    <a href="">#nlw</a>{' '}
-                    <a href="">#rocketseat🚀</a>{' '}
-                </p>
+                {content.map(line => {
+                    if(line.type == 'paragraph') {
+                        return <p>{line.content}</p>;
+                    } else if(line.type == 'link') {
+                        return <p><a href="#">{line.content}</a></p>;
+                    }
+                })}
             </div>
 
             <form className={styles.commentForm}> 
