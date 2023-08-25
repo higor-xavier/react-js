f488ac8d0202d2ea769b3b7b1f8fe6a09d9084a8
higor-xavier <higorsxavier@gmail.com>
2023-08-25 17:57:12 -0300

    Adding new comments to the posts

--- vite-project/src/components/Post.jsx
+++ vite-project/src/components/Post.jsx
@@ -3,8 +3,16 @@ import ptBR from 'date-fns/locale/pt-BR';
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
+import { useState } from 'react';
+
+// estado = variáveis que eu quero que o componente monitore
 
 export function Post({author, publishedAt, content}){
+
+    const [comments, setComments] = useState([
+        1,
+        2,
+    ]);
     
     const publishedDateFormated = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
         locale: ptBR,
@@ -14,6 +22,12 @@ export function Post({author, publishedAt, content}){
         locale: ptBR,
         addSuffix: true
     });
+
+    function handleCreateNewComment() {
+        event.preventDefault();
+        // imutabilidade
+        setComments([...comments, comments.length + 1]);
+    }
 
     return(
         <article className={styles.post}>
@@ -42,7 +56,7 @@ export function Post({author, publishedAt, content}){
                 })}
             </div>
 
-            <form className={styles.commentForm}> 
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}> 
                 <strong>Deixe seu feedback</strong>
                 <textarea 
                     placeholder='Deixe um comentário'
@@ -53,9 +67,9 @@ export function Post({author, publishedAt, content}){
             </form>
 
             <div className={styles.commentList}>
-                <Comment />
-                <Comment />
-                <Comment />
+                {comments.map(comment => {
+                    return <Comment />;
+                })}
             </div>
 
         </article>
