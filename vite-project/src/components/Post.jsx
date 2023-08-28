5e7f584ff439f30b0ee144797d2a7186ab38d7e0
higor-xavier <higorsxavier@gmail.com>
2023-08-27 22:50:51 -0300

    Deleting comments (immutability)

--- vite-project/src/components/Post.jsx
+++ vite-project/src/components/Post.jsx
@@ -37,6 +37,14 @@ export function Post({author, publishedAt, content}){
         setNewCommentText(event.target.value);
     }
 
+    function deleteComment(commentToDelete) {
+        // imutabilidade = as variáveis não sofrem mutação, nós criamos um novo valor(um novo espaço na memória)
+        const commentsWithoutDeletedOne = comments.filter(comment => {
+            return comment !== commentToDelete;
+        })
+        setComments(commentsWithoutDeletedOne);
+    }
+
     return(
         <article className={styles.post}>
             <header>
@@ -79,7 +87,13 @@ export function Post({author, publishedAt, content}){
 
             <div className={styles.commentList}>
                 {comments.map(comment => {
-                    return <Comment key={comment} content={comment}/>;
+                    return (
+                        <Comment 
+                            key={comment} 
+                            content={comment} 
+                            onDeleteComment={deleteComment}
+                        />
+                    );
                 })}
             </div>
 
