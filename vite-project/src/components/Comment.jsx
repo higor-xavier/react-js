e71b0b771740dd7f937eef8d4f5abba42284947e
higor-xavier <higorsxavier@gmail.com>
2023-08-28 16:45:41 -0300

    React closures

--- vite-project/src/components/Comment.jsx
+++ vite-project/src/components/Comment.jsx
@@ -1,11 +1,20 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css';
 import { Avatar } from './Avatar';
+import { useState } from 'react';
 
 export function Comment({content, onDeleteComment}) {
 
+    const [likeCount, setLikeCount] = useState(0);
+
     function handleDeleteComment() {
         onDeleteComment(content);
+    }
+
+    function handleLikeComment() {
+        setLikeCount((state) => {
+            return state + 1;
+        });
     }
 
     return (
@@ -29,9 +38,9 @@ export function Comment({content, onDeleteComment}) {
                 </div>
 
                 <footer>
-                    <button>
+                    <button onClick={handleLikeComment}>
                         <ThumbsUp/>
-                        Apludir <span>20</span>
+                        Apludir <span>{likeCount}</span>
                     </button>
                 </footer>
             </div>
