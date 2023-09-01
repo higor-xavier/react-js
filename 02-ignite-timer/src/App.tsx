f1739fd88c563e076f99c4018a6756af08171242
higor-xavier <higorsxavier@gmail.com>
2023-09-01 19:05:34 -0300

    Global style

--- 02-ignite-timer/src/App.tsx
+++ 02-ignite-timer/src/App.tsx
@@ -1,12 +1,16 @@
-import { ThemeProvider } from 'styled-components'
-import { defaultTheme } from './styles/themes/default'
+import { ThemeProvider } from 'styled-components';
+
+import { defaultTheme } from './styles/themes/default';
+import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
 
     <ThemeProvider theme={defaultTheme}>
       <h1>Hello moto</h1>
+        asassdad
+      <GlobalStyle />
     </ThemeProvider>
 
-  )
+  );
 }
