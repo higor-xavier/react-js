d1fbfd98eebfb68b982846033cb5291b885ae418
higor-xavier <higorsxavier@gmail.com>
2024-03-21 14:19:35 -0300

    Action types and Cycle reducer

--- 02-ignite-timer/src/contexts/CyclesContext.tsx
+++ 02-ignite-timer/src/contexts/CyclesContext.tsx
@@ -1,4 +1,5 @@
 import { ReactNode, createContext, useState, useReducer } from 'react'
+import { ActionTypes, Cycle, cyclesReducer } from '../reducers/cycles'
 // Use reducer para armazenar informações complexas.
 // Geralmente, essas informações precisam ser alteradas futuramente.
 // Útil lembrar que o reducer serve como um local fixo para todas alterações
@@ -7,15 +8,6 @@ import { ReactNode, createContext, useState, useReducer } from 'react'
 interface CreateCycleData {
   task: string
   minutesAmount: number
-}
-
-interface Cycle {
-  id: string
-  task: string
-  minutesAmount: number
-  startDate: Date
-  interruptedDate?: Date
-  finishedDate?: Date
 }
 
 interface CyclesContextType {
@@ -42,18 +34,16 @@ export function CyclesContextProvider({
   // o state -> valor atual da variável de estado, nesse caso o 'cycle'
   // a action -> é a definição da ação que irá alterar a variável de estado
   // useReducer((função(state, action)), valor-inicial-da-variavél) -- nesse caso é o cycles
-  const [cycles, dispatch] = useReducer((state: Cycle[], action: any) => {
-    if (action.type === 'ADD_NEW_CYCLE') {
-      return [...state, action.payload.newCycle]
-    }
+  const [cyclesState, dispatch] = useReducer(cyclesReducer, {
+    cycles: [],
+    activeCycleId: null,
+  })
 
-    return state
-  }, [])
-
-  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
   // Guardando quantidade de tempo que levou da definição até a execução
   // do ciclo
   const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+
+  const { cycles, activeCycleId } = cyclesState
 
   // Variável que guarda o ciclo ativo
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
@@ -64,20 +54,11 @@ export function CyclesContextProvider({
 
   function markCurrentCycleAsFinished() {
     dispatch({
-      type: 'MARK_CURRENT_CYCLE_AS_FINISHED',
+      type: ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED,
       payload: {
         activeCycleId,
       },
     })
-    // setCycles((state) =>
-    //   state.map((cycle) => {
-    //     if (cycle.id === activeCycleId) {
-    //       return { ...cycle, finishedDateDate: new Date() }
-    //     } else {
-    //       return cycle
-    //     }
-    //   }),
-    // )
   }
 
   function createNewCycle(data: CreateCycleData) {
@@ -94,7 +75,7 @@ export function CyclesContextProvider({
     }
 
     dispatch({
-      type: 'ADD_NEW_CYCLE',
+      type: ActionTypes.ADD_NEW_CYCLE,
       payload: {
         newCycle,
       },
@@ -103,27 +84,17 @@ export function CyclesContextProvider({
     // Sempre que uma alteração de estado depender da anterior, deve-se usar o
     // formato de arrowfunction
     // setCycles((state) => [...state, newCycle])
-    setActiveCycleId(id)
+
     setAmountSecondsPassed(0)
   }
 
   function interruptCurrentCycle() {
     dispatch({
-      type: 'INTERRUPT_CURRENT_CYCLE',
+      type: ActionTypes.INTERRUPT_CURRENT_CYCLE,
       payload: {
         activeCycleId,
       },
     })
-    // setCycles((state) =>
-    //   state.map((cycle) => {
-    //     if (cycle.id === activeCycleId) {
-    //       return { ...cycle, interruptedDate: new Date() }
-    //     } else {
-    //       return cycle
-    //     }
-    //   }),
-    // )
-    setActiveCycleId(null)
   }
 
   return (
