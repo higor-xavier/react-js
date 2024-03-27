c3e5b1cb1fa3bfb19aed891130fe94bb44c04dc8
higor-xavier <higorsxavier@gmail.com>
2024-03-27 17:55:48 -0300

    Setting dialog structure

--- 03-dt-money/src/components/Header/index.tsx
+++ 03-dt-money/src/components/Header/index.tsx
@@ -1,4 +1,5 @@
-import { HeaderContainer, HeaderContent, NewTransactionsButton } from "./styles";
+import { HeaderContainer, HeaderContent, NewTransactionsButton } from "./styles"
+import * as Dialog from '@radix-ui/react-dialog'
 
 import logoImg from '../../assets/logo.svg'
 
@@ -8,7 +9,32 @@ export function Header() {
             <HeaderContent>
                 <img src={logoImg} alt="" />
 
-                <NewTransactionsButton>Nova transação</NewTransactionsButton>
+                <Dialog.Root>
+                    {/* O trigger é responsável por abrir o modal */}
+                    {/* Nesse caso, a propriedade 'asChild' "passa" essa função 
+                        pro componente interno
+                    */}
+                    <Dialog.Trigger asChild>
+                        <NewTransactionsButton>Nova transação</NewTransactionsButton>
+                    </Dialog.Trigger>
+
+                    {/* O 'Portal' tem a funcionalidade de transportar a informação
+                        do modal para outro local, uma vez que o modal não pertence
+                        aos elementos onde está inserido. Ele está sobre esses 
+                        elementos
+                    */}
+                    <Dialog.Portal>
+                        {/* O overlay é só o background transparente mesmo */}
+                        <Dialog.Overlay />
+
+                        {/* Conteúdo do modal */}
+                        <Dialog.Content>
+                            {/* Importante adicionar o title para os leitores de tela */}
+                            <Dialog.Title>Nova transação</Dialog.Title>
+                            <Dialog.Close />
+                        </Dialog.Content>
+                    </Dialog.Portal>
+                </Dialog.Root>
             </HeaderContent>
         </HeaderContainer>
     )
