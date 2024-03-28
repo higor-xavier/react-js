0d629729c717a030f36b7ad187cbcd962b5a1ee9
higor-xavier <higorsxavier@gmail.com>
2024-03-28 12:41:49 -0300

    API consumption

--- 03-dt-money/src/pages/Transactions/index.tsx
+++ 03-dt-money/src/pages/Transactions/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
@@ -5,6 +6,35 @@ import { SearchForm } from "./components/SearchForm";
 import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
+
+    interface Transaction {
+        id: number;
+        description: string;
+        type: 'income' | 'outcome';
+        price: number;
+        category: string;
+        createdAt: string;
+    }
+
+    // Vamos usar um estado para armazenar e mostrar as informações obtidas da API
+    // Sempre lembrar de tipar os estados no react
+    const [transactions, setTransactions] = useState<Transaction[]>([])
+
+    // Sempre que o componente entrar em um novo fluxo de renderização, o fetch será executado.
+    // Sendo assim, faz-se necessário acrescentar o useEffect com array de dependências vazio
+    // para que seja executado apenas uma vez
+
+    async function loadTransactions() {
+        const response = await fetch('http://localhost:3000/transactions')
+        const data = await response.json();
+
+        setTransactions(data);
+    }
+
+    useEffect(() => {
+        loadTransactions();
+    }, []) 
+
     return (
         <div>
             <Header />
@@ -15,27 +45,20 @@ export function Transactions() {
 
                 <TransactionsTable>
                     <tbody>
-                        <tr>
-                            <td width="50%">Desenvolvimento de site</td>
-                            <td>
-                                <PriceHighLight variant="income">
-                                    R$ 12.000,00
-                                </PriceHighLight>
-                            </td>
-                            <td>Venda</td>
-                            <td>13/04/2023</td>
-                        </tr>
-
-                        <tr>
-                            <td width="50%">Hamburguer</td>
-                            <td>
-                                <PriceHighLight variant="outcome">
-                                    - R$ 59,00
-                                </PriceHighLight>
-                            </td>
-                            <td>Alimentação</td>
-                            <td>10/04/2023</td>
-                        </tr>
+                        {transactions.map(transaction => {
+                            return (
+                                <tr key={transaction.id}>
+                                    <td width="50%">{transaction.description}</td>
+                                    <td>
+                                        <PriceHighLight variant={transaction.type}>
+                                            {transaction.price}
+                                        </PriceHighLight>
+                                    </td>
+                                    <td>{transaction.category}</td>
+                                    <td>{transaction.createdAt}</td>
+                                </tr>
+                            )
+                        })}
                     </tbody>
                 </TransactionsTable>
             </TransactionsContainer>
