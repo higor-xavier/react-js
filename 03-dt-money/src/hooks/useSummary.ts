1fac34d79ba7f77a8ea69ccc8c986c9ae2bec178
higor-xavier <higorsxavier@gmail.com>
2024-04-02 16:06:50 -0300

    Using context selectors

--- 03-dt-money/src/hooks/useSummary.ts
+++ 03-dt-money/src/hooks/useSummary.ts
@@ -1,8 +1,10 @@
-import { useContext } from 'react'
 import { TransactionsContext } from '../contexts/TransactionsContext'
+import { useContextSelector } from 'use-context-selector'
 
 export function useSummary() {
-  const { transactions } = useContext(TransactionsContext)
+  const transactions = useContextSelector(TransactionsContext, (context) => {
+    return context.transactions
+  })
 
   // reduce() percorre um array e o reduz a uma nova estrutura de dados
   // ou seja, reduzir as informações vindas de [transactions] a essas abaixo
