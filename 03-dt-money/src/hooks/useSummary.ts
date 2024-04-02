fc26b3d4e1df5c41ce2dffc78892c7fb4d4300ec
higor-xavier <higorsxavier@gmail.com>
2024-04-02 17:05:33 -0300

    Using memo for the summary calc

--- 03-dt-money/src/hooks/useSummary.ts
+++ 03-dt-money/src/hooks/useSummary.ts
@@ -1,5 +1,6 @@
 import { TransactionsContext } from '../contexts/TransactionsContext'
 import { useContextSelector } from 'use-context-selector'
+import { useMemo } from 'react'
 
 export function useSummary() {
   const transactions = useContextSelector(TransactionsContext, (context) => {
@@ -9,23 +10,25 @@ export function useSummary() {
   // reduce() percorre um array e o reduz a uma nova estrutura de dados
   // ou seja, reduzir as informações vindas de [transactions] a essas abaixo
   // { income: 0, outcome: 0, total: 0 }
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === 'income') {
-        acc.income += transaction.price
-        acc.total += transaction.price
-      } else {
-        acc.outcome += transaction.price
-        acc.total -= transaction.price
-      }
-      return acc
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  )
+  const summary = useMemo(() => {
+    return transactions.reduce(
+      (acc, transaction) => {
+        if (transaction.type === 'income') {
+          acc.income += transaction.price
+          acc.total += transaction.price
+        } else {
+          acc.outcome += transaction.price
+          acc.total -= transaction.price
+        }
+        return acc
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    )
+  }, [transactions])
 
   return summary
 }
