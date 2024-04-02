d08672aa3f195079336de26f474f2db42e5a7aec
higor-xavier <higorsxavier@gmail.com>
2024-04-02 16:30:45 -0300

    Applying useCallback for performance

--- 03-dt-money/src/contexts/TransactionsContext.tsx
+++ 03-dt-money/src/contexts/TransactionsContext.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect, useState } from 'react'
+import { ReactNode, useEffect, useState, useCallback } from 'react'
 import { api } from '../lib/axios'
 import { createContext } from 'use-context-selector'
 
@@ -39,7 +39,7 @@ export function TransactionsProvider({ children }: TransactionProviderProps) {
   // Sendo assim, faz-se necessário acrescentar o useEffect com array de dependências vazio
   // para que seja executado apenas uma vez
 
-  async function fetchTransactions(query?: string) {
+  const fetchTransactions = useCallback(async (query?: string) => {
     const response = await api.get('/transactions', {
       // necessário await, para não haver promise de response
       params: {
@@ -50,21 +50,29 @@ export function TransactionsProvider({ children }: TransactionProviderProps) {
     })
 
     setTransactions(response.data)
-  }
+  }, [])
 
-  async function createTransaction(data: CreateTransactionInput) {
-    const { description, price, category, type } = data
+  const createTransaction = useCallback(
+    async (data: CreateTransactionInput) => {
+      const { description, price, category, type } = data
 
-    const response = await api.post('/transactions', {
-      description,
-      price,
-      category,
-      type,
-      createdAt: new Date(),
-    })
+      const response = await api.post('/transactions', {
+        description,
+        price,
+        category,
+        type,
+        createdAt: new Date(),
+      })
 
-    setTransactions((state) => [response.data, ...state])
-  }
+      setTransactions((state) => [response.data, ...state])
+    },
+    // [vazio] faz com que essa função não seja recriada em memória
+    // se a função interna precisar de informações de fora,
+    // deve haver de forma explícita no array de dependência para
+    // que não seja utilizada a informação desatualizada da variável
+    // já que se utiliza a informação armazenada em memória anteriormente
+    [],
+  )
 
   useEffect(() => {
     fetchTransactions()
