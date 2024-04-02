5e9c870091850f7aeff3ba32900220b2547ca2fa
higor-xavier <higorsxavier@gmail.com>
2024-04-02 16:54:05 -0300

    How and why to use memo

--- 03-dt-money/src/contexts/TransactionsContext.tsx
+++ 03-dt-money/src/contexts/TransactionsContext.tsx
@@ -76,7 +76,7 @@ export function TransactionsProvider({ children }: TransactionProviderProps) {
 
   useEffect(() => {
     fetchTransactions()
-  }, [])
+  }, [fetchTransactions])
 
   return (
     <TransactionsContext.Provider
