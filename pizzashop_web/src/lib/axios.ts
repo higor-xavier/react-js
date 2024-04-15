44de6ceee47084653d3c97e38911275dd15b87ca
higor-xavier <higorsxavier@gmail.com>
2024-04-15 16:56:15 -0300

    Adding loading state

--- pizzashop_web/src/lib/axios.ts
+++ pizzashop_web/src/lib/axios.ts
@@ -6,3 +6,13 @@ export const api = axios.create({
   baseURL: env.VITE_API_URL,
   withCredentials: true,
 })
+
+// Usando interceptor para interceptar todas as requisições afim de adicionar delay na aplicação
+if (env.VITE_ENABLE_API_DELAY) {
+  // Config são os dados de cada requisção que passa por aqui
+  api.interceptors.request.use(async (config) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+
+    return config
+  })
+}
