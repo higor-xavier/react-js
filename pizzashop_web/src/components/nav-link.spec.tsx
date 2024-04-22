51b7ce619cea07c9140593d1faf454794334209d
higor-xavier <higorsxavier@gmail.com>
2024-04-22 18:14:49 -0300

    navLink test

---
+++ pizzashop_web/src/components/nav-link.spec.tsx
@@ -0,0 +1,27 @@
+import { render } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+
+import { NavLink } from './nav-link'
+
+// Não consigo testar sem estar encapsulado pelo provider de rotas
+describe('NavLink', () => {
+  it('should highlight the nav link when is the current page link', () => {
+    // Então é necessário fazer um wrapper desse NavLink com um router
+    const wrapper = render(
+      <>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+      </>,
+      {
+        wrapper: ({ children }) => {
+          return (
+            <MemoryRouter initialEntries={['/about']}>{children}</MemoryRouter>
+          )
+        },
+      },
+    )
+
+    expect(wrapper.getByText('Home').dataset.current).toEqual('false')
+    expect(wrapper.getByText('About').dataset.current).toEqual('true')
+  })
+})
