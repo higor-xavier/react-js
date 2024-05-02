4f1d76c5211e35023e76f4560db75ae5b73e13af
higor-xavier <higorsxavier@gmail.com>
2024-05-02 18:04:56 -0300

    Price format

--- 04-ignite-shop/src/pages/index.tsx
+++ 04-ignite-shop/src/pages/index.tsx
@@ -36,7 +36,7 @@ export default function Home({ products }: HomeProps) {
 
             <footer>
               <strong>{product.name}</strong>
-              <span>{(product.price) / 100}</span>
+              <span>{product.price}</span>
             </footer>
           </Product>
         )
@@ -53,12 +53,14 @@ export const getStaticProps: GetStaticProps = async () => {
 
   const products =  response.data.map(product => {
     const price = product.default_price as Stripe.Price
-
     return {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      price: price.unit_amount,
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      }).format(price.unit_amount/100),
     }
   })
 
