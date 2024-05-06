97d91b9a68b4ae321082f8fdfbc2b8638e0d3eba
higor-xavier <higorsxavier@gmail.com>
2024-05-06 13:41:30 -0300

    Next Link

--- 04-ignite-shop/src/pages/index.tsx
+++ 04-ignite-shop/src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+import Link from "next/link";
 import { useKeenSlider } from 'keen-slider/react';
 
 import { HomeContainer, Product } from "@/styles/pages/home";
@@ -31,14 +31,20 @@ export default function Home({ products }: HomeProps) {
 
       {products.map(product => {
         return (
-          <Product key={product.id} className="keen-slider__slide">
-            <Image src={product.imageUrl} width={520} height={480} alt="" />
-
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+          <Link
+            key={product.id}
+            href={`/product/${product.id}`}
+            passHref
+            legacyBehavior
+          >
+            <Product className="keen-slider__slide">
+              <Image src={product.imageUrl} width={520} height={480} alt="" />
+                <footer>
+                  <strong>{product.name}</strong>
+                  <span>{product.price}</span>
+                </footer>
+            </Product>
+          </Link>
         )
       })}
       
