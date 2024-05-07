70db5feee232201949cab668575be4e22740fef1
higor-xavier <higorsxavier@gmail.com>
2024-05-07 20:08:37 -0300

    Story: Button config

--- 05-design-system/packages/docs/.storybook/main.ts
+++ 05-design-system/packages/docs/.storybook/main.ts
@@ -10,7 +10,10 @@ function getAbsolutePath(value: string): any {
   return dirname(require.resolve(join(value, "package.json")));
 }
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: [
+    "../src/stories/**/*.stories.mdx",
+    "../src/stories/**/*.stories.tsx"
+  ],
   addons: [
     getAbsolutePath("@storybook/addon-onboarding"),
     getAbsolutePath("@storybook/addon-links"),
