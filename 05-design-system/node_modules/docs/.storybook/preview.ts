cbabcdeb148ee183706bff5594d1b95995cca8b2
higor-xavier <higorsxavier@gmail.com>
2024-05-07 20:25:27 -0300

    Installing Roboto font

--- 05-design-system/node_modules/docs/.storybook/preview.ts
+++ 05-design-system/node_modules/docs/.storybook/preview.ts
@@ -1,12 +1,19 @@
 import type { Preview } from "@storybook/react";
+import {themes} from '@storybook/theming'
 
 const preview: Preview = {
   parameters: {
+    docs: {
+      theme: themes.dark, //aqui está a solução
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+    },
+    backgrounds: {
+      default: 'dark'
     },
   },
 };
