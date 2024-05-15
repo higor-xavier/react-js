356197b8ee17e07cc632406d6ff0d0e0354d9b95
higor-xavier <higorsxavier@gmail.com>
2024-05-15 18:52:19 -0300

    Component Controls

--- 05-design-system/packages/docs/src/stories/Avatar.stories.tsx
+++ 05-design-system/packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
         src: 'http://github.com/higor-xavier.png',
         alt: 'Higor Xavier',
     },
+    argTypes: {
+        src: {
+            control: {
+                type: 'text',
+            },
+        },
+    },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
--- 05-design-system/packages/docs/src/stories/Text.stories.tsx
+++ 05-design-system/packages/docs/src/stories/Text.stories.tsx
@@ -5,7 +5,16 @@ export default {
     title: 'Typography/Text',
     component: Text,
     args: {
-        children: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel minus sunt tempore quod distinctio blanditiis deserunt unde error expedita magni molestias ducimus placeat itaque dignissimos doloribus quia nemo, ad fugiat.'
+        children: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel minus sunt tempore quod distinctio blanditiis deserunt unde error expedita magni molestias ducimus placeat itaque dignissimos doloribus quia nemo, ad fugiat.',
+        size: 'md',
+    },
+    argTypes: {
+        size: {
+            options: ['xxs','xs','sm','md','lg','xl','2xl','4xl','5xl','6xl','7xl','8xl','9xl'],
+            control: {
+                type: 'inline-radio',
+            }
+        },
     },
 } as Meta<TextProps>
 
